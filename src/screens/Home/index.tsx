9b23160c3592b3000ab817bfee60b8c827eda81e
Fellipe Barros <fellipe.barros@ztech.net>
2021-12-17 11:11:57 -0300

    rollback camera

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,11 @@
-import { useNavigation } from '@react-navigation/native';
 import React, { useEffect, useState } from 'react';
-import { Text, Touchable, TouchableOpacity } from 'react-native';
+import { Text } from 'react-native';
 
 import Container from '../../components/Container';
 import { SafeContainer } from '../../components/Container/styles';
 import { getUser, IUser } from '../../utils/user';
 
 const Home = () => {
-  const navigation = useNavigation<any>();
   const [user, setUser] = useState<IUser | any>();
 
   const getUserStorage = async () => {
@@ -20,25 +18,11 @@ const Home = () => {
     getUserStorage();
   }, []);
 
-  const goTo = (screen: string, stack?: string) => {
-    if (stack) {
-      return navigation.navigate(stack, { screen });
-    }
-
-    return navigation.navigate(screen);
-  };
-
   return (
     <>
       <SafeContainer>
         <Container>
           <Text>Bem-vindo {user?.name}</Text>
-          <TouchableOpacity
-            onPress={() =>
-              goTo('ServiceOrdersScreen', 'ServiceOrdersStackScreen')
-            }>
-            <Text>sss</Text>
-          </TouchableOpacity>
         </Container>
       </SafeContainer>
     </>
